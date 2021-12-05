21792479ef37501fbd5aa56ea5ab87932ba0a9c3
Astrorikoo <2019141460048@stu.scu.edu.cn>
2021-12-05 15:41:37 +0800

    2

--- 第五次作业/scudb_initial/cmake-build-debug/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/leter1/Database_Project/scudb_initial/scudb_initial/src
-# Build directory: /home/leter1/Database_Project/scudb_initial/scudb_initial/cmake-build-debug/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
